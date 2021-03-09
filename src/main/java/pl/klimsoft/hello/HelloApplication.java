badc7a6b6ae1b17b5276c3ba1444f8d0d3cc6213
klimsoft <kamil.klimaszewski@gmail.com>
2021-03-09 09:33:39 +0100

    fix

--- src/main/java/pl/klimsoft/hello/HelloApplication.java
+++ src/main/java/pl/klimsoft/hello/HelloApplication.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Maven at " + new Date())
+		System.out.println("Hello Maven at " + new Date());
 	}
 
 }
