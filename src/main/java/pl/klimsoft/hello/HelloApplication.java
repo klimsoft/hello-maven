d90e94063c6e35545b124b3eb940d02208097536
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2021-11-13 23:13:28 +0100

    LocalDateTime

--- src/main/java/pl/klimsoft/hello/HelloApplication.java
+++ src/main/java/pl/klimsoft/hello/HelloApplication.java
@@ -1,12 +1,12 @@
 package pl.klimsoft.hello;
 
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Maven at " + new Date());
+		System.out.println("Hello Maven at " + LocalDateTime.now());
 	}
 
 }
