f07eed9345a10aea3eb9e470af6d72dbb3a5f9ba
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2021-11-20 19:37:45 +0100

    Feature 2

--- src/main/java/pl/klimsoft/hello/HelloApplication.java
+++ src/main/java/pl/klimsoft/hello/HelloApplication.java
@@ -7,6 +7,7 @@ public class HelloApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Maven at " + LocalDateTime.now());
+		System.out.println("Additional feature 2");
 	}
 
 }
